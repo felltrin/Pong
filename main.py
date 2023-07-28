bdd2e7351328d5c3f618490004a78438d01552dd
Lucius Holzinger <luciholzinger@gmail.com>
2023-07-28 14:56:18 -0700

    drew the two player rectangles and added the movement for them

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import pygame as py
 
 
+def player_movement(player_one_pos, player_two_pos, dt):
+    keys = py.key.get_pressed()
+    if keys[py.K_w]:
+        player_one_pos.y -= 300 * dt
+    if keys[py.K_s]:
+        player_one_pos.y += 300 * dt
+    if keys[py.K_DOWN]:
+        player_two_pos.y += 300 * dt
+    if keys[py.K_UP]:
+        player_two_pos.y -= 300 * dt
+
+
+def draw_player(player_pos, screen):
+    py.draw.rect(screen, "black", py.Rect(player_pos, (20, 100)))
+
+
 def main():
     py.init()
     screen = py.display.set_mode((1280, 720))
     clock = py.time.Clock()
     running = True
+    dt = 0
+
+    player_one_pos = py.Vector2(75, screen.get_height() / 2)
+    player_two_pos = py.Vector2(1200, screen.get_height() / 2)
 
     while running:
         for event in py.event.get():
@@ -14,9 +34,13 @@ def main():
 
         screen.fill("green")
 
+        draw_player(player_one_pos, screen)
+        draw_player(player_two_pos, screen)
+        player_movement(player_one_pos, player_two_pos, dt)
+
         py.display.flip()
 
-        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
     py.quit()
 
