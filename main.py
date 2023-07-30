c437bf4c23e7e4932d7750da7801c7759db46263
Lucius Holzinger <luciholzinger@gmail.com>
2023-07-30 10:55:11 -0700

    ball bounces off players

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import pygame as py
 
 
-def ball_movement():
-    py.draw.rect(screen, "black", py.Rect(ball_pos, (20, 20)))
-    ball_pos.x += 5
-
-
 def player_movement(player_one_pos, player_two_pos, dt):
     keys = py.key.get_pressed()
     if keys[py.K_w]:
@@ -30,7 +25,9 @@ def player_constraint(player_one_pos, player_two_pos):
 
 
 def draw_player(player_pos, screen):
-    return py.draw.rect(screen, "black", py.Rect(player_pos, (20, 100)))
+    player_rect = py.Rect(player_pos, (20, 100))
+    py.draw.rect(screen, "black", py.Rect(player_pos, (20, 100)))
+    return player_rect
 
 
 def draw_divider(screen):
@@ -51,11 +48,14 @@ if __name__ == '__main__':
     clock = py.time.Clock()
     running = True
     dt = 0
+    direction_x_speed = 5
+    direction_y_speed = 5
 
     my_font = py.font.SysFont("Sans Serif", 108)
     player_one_pos = py.Vector2(30, screen.get_height() / 2)
     player_two_pos = py.Vector2(1230, screen.get_height() / 2)
     ball_pos = py.Vector2(screen.get_width() / 2, 0)
+    ball_rect = py.Rect(ball_pos, (20, 20))
 
     player_one_num, player_two_num = 0, 0
     player_one_score = my_font.render(str(player_one_num), 1, "black")
@@ -74,7 +74,17 @@ if __name__ == '__main__':
         player_movement(player_one_pos, player_two_pos, dt)
         player_constraint(player_one_pos, player_two_pos)
         draw_divider(screen)
-        ball_movement()
+
+        # ball movement
+        ball_rect.x += direction_x_speed
+        ball_rect.y += direction_y_speed
+        if player_two_rect.colliderect(ball_rect) or player_one_rect.colliderect(ball_rect):
+            direction_x_speed *= -1
+        if ball_rect.top <= 0 or ball_rect.bottom >= screen.get_height():
+            direction_y_speed *= -1
+        if ball_rect.left <= 0 or ball_rect.right >= screen.get_width():
+            direction_x_speed *= -1
+        py.draw.rect(screen, "black", ball_rect)
 
         py.display.flip()
 
