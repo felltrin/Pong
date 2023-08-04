9b54876828389b935207622179c4b322636d79bb
Lucius Holzinger <luciholzinger@gmail.com>
2023-08-04 10:41:59 -0700

    players can now score against each other; the game closes once one person reaches five points; ball position resets upon scoring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as py
+import random as rand
 
 
 def player_movement(player_one_pos, player_two_pos, dt):
@@ -26,7 +27,7 @@ def player_constraint(player_one_pos, player_two_pos):
 
 def draw_player(player_pos, screen):
     player_rect = py.Rect(player_pos, (20, 100))
-    py.draw.rect(screen, "black", py.Rect(player_pos, (20, 100)))
+    py.draw.rect(screen, "black", player_rect)
     return player_rect
 
 
@@ -42,6 +43,16 @@ def draw_score(screen):
     screen.blit(player_two_score, ((screen.get_width() / 4) * 3, 30))
 
 
+# reset ball position
+# which entails picking a random point along the middle
+def reset_game(screen):
+    ball_position_num = rand.randint(0, 14)
+    ball_y_position = 15 * ball_position_num
+    new_ball_pos = py.Vector2(screen.get_width() / 2, ball_y_position)
+    new_ball_rect = py.Rect(new_ball_pos, (20, 20))
+    return new_ball_rect
+
+
 if __name__ == '__main__':
     py.init()
     screen = py.display.set_mode((1280, 720))
@@ -50,6 +61,7 @@ if __name__ == '__main__':
     dt = 0
     direction_x_speed = 5
     direction_y_speed = 5
+    start_x_direction = True
 
     my_font = py.font.SysFont("Sans Serif", 108)
     player_one_pos = py.Vector2(30, screen.get_height() / 2)
@@ -82,12 +94,33 @@ if __name__ == '__main__':
             direction_x_speed *= -1
         if ball_rect.top <= 0 or ball_rect.bottom >= screen.get_height():
             direction_y_speed *= -1
-        if ball_rect.left <= 0 or ball_rect.right >= screen.get_width():
-            direction_x_speed *= -1
+        if ball_rect.left <= 0:
+            player_two_num += 1
+            player_two_score = my_font.render(str(player_two_num), 1, "black")
+            ball_rect = reset_game(screen)
+            if start_x_direction:
+                start_x_direction = False
+                direction_x_speed = -5
+            else:
+                start_x_direction = True
+                direction_x_speed = 5
+        if ball_rect.left >= screen.get_width():
+            player_one_num += 1
+            player_one_score = my_font.render(str(player_one_num), 1, "black")
+            ball_rect = reset_game(screen)
+            if start_x_direction:
+                start_x_direction = False
+                direction_x_speed = -5
+            else:
+                start_x_direction = True
+                direction_x_speed = 5
+
         py.draw.rect(screen, "black", ball_rect)
 
-        py.display.flip()
+        if player_one_num == 5 or player_two_num == 5:
+            running = False
 
+        py.display.flip()
         dt = clock.tick(60) / 1000
 
     py.quit()
